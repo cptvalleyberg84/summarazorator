b987edcd0177cb5f3e25a96ad4ec4d9905459efe
Rafal Zygula <p0hmgxcn@students.codeinstitute.net>
2024-11-04 18:08:19 +0000

    Add edit and delete comment option

--- about/views.py
+++ about/views.py
@@ -1,9 +1,18 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
 from .forms import CollaborateForm
 
 # Create your views here.
 def about_srzr(request):
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(request, messages.SUCCESS,
+            "Collaboration request received! hank you for interest. We will get back to you as soon as we can.")
+
     """
     Renders the About Summarazorator page
     """
