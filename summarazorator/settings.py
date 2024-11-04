8377106af81b91a62e476d5935b04256f4a38b90
Rafal Zygula <p0hmgxcn@students.codeinstitute.net>
2024-11-04 14:59:16 +0000

    Add allauth

--- summarazorator/settings.py
+++ summarazorator/settings.py
@@ -33,9 +33,11 @@ DEBUG = False
 ALLOWED_HOSTS = ['8000-cptvalleybe-summarazora-s50rt81q064.ws.codeinstitute-ide.net',
                 '.herokuapp.com']
 
+SITE_ID = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -43,6 +45,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
     'django_summernote',
     'forum',
     'about',
@@ -57,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'allauth.account.middleware.AccountMiddleware',
 ]
 
 ROOT_URLCONF = 'summarazorator.urls'
@@ -113,6 +120,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
