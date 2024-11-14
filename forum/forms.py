b89f52312bfcae1f62c0380ab1ba74560be08423
Rafal Zygula <p0hmgxcn@students.codeinstitute.net>
2024-11-14 13:47:45 +0000

    Add comment minimum characters requirement

--- forum/forms.py
+++ forum/forms.py
@@ -1,8 +1,11 @@
 from .models import Comment
 from django import forms
+from django.core.validators import MinLengthValidator
 
 
 class CommentForm(forms.ModelForm):
+    body = forms.CharField(validators=[MinLengthValidator(12)])
     class Meta:
         model = Comment
         fields = ('body',)
+
